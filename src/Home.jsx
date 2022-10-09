ea93cc083228e01dfbe76db6143eccbd82b91836
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-09 23:03:46 +0700

    add: outputing list

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
+    const [blogs, setBlos] = useState([
+        {
+            title: 'blog pertama',
+            body: 'ini merupakan blog pertama saya',
+            author: 'reza',
+            id: 1
+        },
+        {
+            title: 'blog kedua',
+            body: 'ini merupakan blog kedua saya',
+            author: 'ifran',
+            id: 2
+        },
+        {
+            title: 'blog ketiga',
+            body: 'ini merupakan blog ketiga saya',
+            author: 'wijaya',
+            id: 3
+        },
+    ]);
+
+    return (
         <div className="home">
-            <h1>Homepage</h1>
+            {/* foreach nya jsx */}
+            {blogs.map((blog) => (
+                <div className="blog-preview" id={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
+                    <p>by: {blog.author}</p>
+                </div>
+            ))}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
