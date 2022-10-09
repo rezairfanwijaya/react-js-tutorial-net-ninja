8a63bc818344febf01c1353714177e95f645d0e3
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-09 12:36:33 +0700

    add: more value to component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 function App() {
   const title = 'My First Blog'
   const likes = 230
+  const scores = [90,80,78,65,99]
+  const person = {name:'reza', level:12}
   return (
     <div className="App">
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} time</p>
+        <h1>{ title }</h1>
+        <p>Liked { likes } time</p>
+        <p>{ 'my blog' }</p>
+        <p>{ 10 }</p>
+        <p>{ [1,2,3,4,5,6] }</p>
+        <p>{ scores[4] }</p>
+        <p>{ Math.random() * 120 }</p>
       </div>
     </div>
   );
