6fcea8429fb03f281891b28602f78c42a054055d
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-09 12:06:21 +0700

    add: values to component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'My First Blog'
+  const likes = 230
   return (
     <div className="App">
       <div className="content">
-        <h1>Hello World</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} time</p>
       </div>
     </div>
   );
