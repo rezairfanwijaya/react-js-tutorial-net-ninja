cb9dfd6c65aa5d61c68081d62d302f218600c16d
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-09 14:09:12 +0700

    add: event

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,32 @@
 const Home = () => {
+    // function ini akan dipanggil ketika pertama kali halaman dibuka
+    // atau halaman di reload
+    // tanpa kita mengklik tombol nya
+    // jika pada pemanggilan function menggunakan ()
+    // bisa dilihat pada button Click Me
+    const handleClick = (e) => {
+        alert('Hallo Antusism')
+    }
+
+    // function ini akan dipanggil ketika kita hanya menekan tombol saja
+    // tidak terpengaruh ketika membuka halaman atau merefresh halaman 
+    // seperti function handleClick diatas
+    const handleClickAgain = (name, e) =>{
+        if( name === undefined ){
+            alert('Hallo admin')
+        }else {
+            console.log('Hallo ' + name + e.target)
+        }
+    }
+
     return ( 
         <div className="home">
             <h1>Homepage</h1>
+            {/* pemanggilan event tanpa menggunakan anonymous function */}
+            <button onClick={ handleClick() }>Click Me</button>
+
+            {/* pemanggilan event menggunakan anonymout function */}
+            <button onClick={ (e)=>handleClickAgain('reza', e) }>Click Me Again</button>
         </div>
      );
 }
