0eb92c0f0c9498107f4f31aa8d3e81fd4899453d
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-15 13:08:40 +0700

    add: key

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -5,8 +5,10 @@ const BlogList = ({ blogs, titleSection }) => {
             <h2 className="title-section-blog">
                 {titleSection}
             </h2>
+
             {blogs.map((blog) => (
-                <div className="blog-preview" id={blogs.id}>
+                <div className="blog-preview" id={blogs.id}
+                    key={blogs.id}>
                     <h3>{blog.title}</h3>
                     <p>{blog.body}</p>
                     <p>{blog.author}</p>
