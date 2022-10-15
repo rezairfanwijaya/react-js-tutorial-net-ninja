fa1e5dfc139874841be233ed6ef5dee2358eec0c
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-15 22:56:02 +0700

    add: error handling

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,17 +4,26 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState('');
     const [isLoading, setIsLoading] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('')
 
     // fetching data menggunakan useEffect
     useEffect(() => {
-       setTimeout(()=>(
-        fetch('http://localhost:8080/blogs')
-        .then(res => { return res.json() })
-        .then(
-            data => setBlogs(data),
-            setIsLoading(false)
-        )
-       ),3000)
+        setTimeout(() => (
+            fetch('http://localhost:8080/blogs')
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error(`gagal mengambil data dari server. status code : ${res.status}`);
+                    }
+                    return res.json()
+                })
+                .then(
+                    data => setBlogs(data),
+                    setIsLoading(false)
+                )
+                .catch(e => {
+                    setErrorMessage(e.message)
+                })
+        ), 3000)
     }, []);
 
     return (
@@ -26,7 +35,9 @@ const Home = () => {
             {
                 isLoading && <div className="loading">Loading.......</div>
             }
-
+            {
+                errorMessage && <div className="loading">{errorMessage}</div>
+            }
             {
                 blogs &&
                 <BlogList dataBlogs={blogs} titleHeader="All blogs" />
