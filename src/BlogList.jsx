4d24258b28999c2089555bd3189110e4e01e8ec1
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-15 12:45:41 +0700

    refactor

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,13 +1,15 @@
-const BlogList = ( {blogs, titleSection} ) => {
+const BlogList = ({ blogs, titleSection }) => {
     return (
         <>
             {/* foreach nya jsx */}
-            <h2 className="title-section-blog">{ titleSection }</h2>
+            <h2 className="title-section-blog">
+                {titleSection}
+            </h2>
             {blogs.map((blog) => (
-                <div className="blog-preview" id={ blogs.id }>
-                    <h3>{ blog.title }</h3>
-                    <p>{ blog.body }</p>
-                    <p>{ blog.author }</p>
+                <div className="blog-preview" id={blogs.id}>
+                    <h3>{blog.title}</h3>
+                    <p>{blog.body}</p>
+                    <p>{blog.author}</p>
                 </div>
             ))}
         </>
