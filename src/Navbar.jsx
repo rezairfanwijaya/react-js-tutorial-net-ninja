ed62dd0f64cbcf1b20add0fb2f8838555de2a678
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-16 19:22:15 +0700

    add: link to in react router

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,30 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return ( 
+    return (
         <nav className="navbar">
             <div className="left-side">
                 <h1>Abdas Blog</h1>
             </div>
 
             <div className="right-side">
-                <a href="/">Home</a>
+                {/* <a href="/">Home</a>
                 <a href="/create" style={{
-                    color : "white",
-                    borderRadius : "6px"
-                }} className="create-blog">Add Blog</a>
+                    color: "white",
+                    borderRadius: "6px"
+                }} className="create-blog">Add Blog</a> */}
+                {/* agar terlihat lebih react kita bisa menggunakan link to 
+                instead kita menggunakan a href.
+                value dari to bisa dilihat dan diatur pada file app.js
+                 */}
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
+                    color: "white",
+                    borderRadius: "6px"
+                }} className="create-blog">Add Blog</Link>
             </div>
         </nav>
-     );
+    );
 }
- 
+
 export default Navbar;
